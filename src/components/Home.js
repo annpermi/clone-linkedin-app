67061fc9424722308b9df3156d2861ee96de9319
annpermi <perminova.anna85@gmail.com>
2021-09-29 14:45:18 -0400

    LeftSide component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import LeftSide from './LeftSide';
+import RightSide from "./RightSide";
+import Main from "./Main";
 
 const Home = (props) => {
     return (
@@ -13,9 +16,9 @@ const Home = (props) => {
                 </p>
             </Section>
             <Layout>
-                <div>Left side</div>
-                <div>middle side</div>
-                <div>right side</div>
+                <LeftSide />
+                <Main />
+                <RightSide />
             </Layout>
         </Container>
     )
